35653fe012d723253c6eed2589ab9a8f185576c2
Meg Clarke <meggyclarke@gmail.com>
2016-10-18 14:30:16 -0400

    Hello Worlds

--- exercise1.rb
+++ exercise1.rb
@@ -10,3 +10,8 @@ puts "Hello World"
 puts "Betty\'s pie shop"
 
 puts "Meg lived in Ireland for #{2008 - 2005} years."
+
+puts "Hello\t\tworld"
+puts "Hello\b\b\b\b\bGoodbye world"
+puts "Hello\rStart over world"
+puts "1. Hello\n2. World"
