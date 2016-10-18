a1ddfdad16c9e7ee1634e7dd8a5cafeea92495a3
Meg Clarke <meggyclarke@gmail.com>
2016-10-18 15:34:16 -0400

    How Old Are You

--- exercise3.rb
+++ exercise3.rb
@@ -1,4 +1,8 @@
 puts "What is your name?"
 name = gets.chomp
 
-puts "Hi #{name}!"
+puts "Hi #{name}! How old are you?"
+age = gets.chomp
+age_new = age.to_i
+
+puts "I remember #{2016 - age_new}, Good Bordeaux Vintages that year."
